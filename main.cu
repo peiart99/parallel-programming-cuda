b3ef11d8419ad1344bdaac2374e17e8bb7d7d9a5
peiart99 <peiart99@gmail.com>
2022-12-24 12:06:35 +0100

    added GPU code

--- main.cu
+++ main.cu
@@ -100,6 +100,52 @@ void calculateSquare(int array_length, int reach, int central_i, int central_j, 
     }
 }
 
+__global__ void deviceCalculateAnswer(int array_length, int reach, float *data_array, float *out_array)
+{
+    //printf("block id: %d thread id: %d\n", blockIdx.x, threadIdx.x);
+    int tid = threadIdx.x;
+    int bid = blockIdx.x;
+
+    int upper_left_index_j {0};
+    int upper_left_index_i {0};
+    int square_length = 2 * reach + 1;
+    int lower_edge {0};
+    int right_edge {0};
+    int diff_j {0};
+    int diff_i {0};
+    if(tid - reach >= 0)
+    {
+        upper_left_index_j = tid - reach;
+    }else
+    {
+        upper_left_index_j = 0;
+    }
+    diff_j = upper_left_index_j - (tid - reach);
+    right_edge = upper_left_index_j - diff_j + square_length;
+    if(right_edge > array_length)
+    {
+        right_edge = array_length;
+    }
+
+    if(bid - reach >= 0)
+    {
+        upper_left_index_i = bid - reach;
+    }else
+    {
+        upper_left_index_i - 0;
+    }
+    diff_i = upper_left_index_i - (bid - reach);
+    lower_edge = upper_left_index_i - diff_i + square_length;
+
+    for(int i {upper_left_index_i}; i < lower_edge; i++)
+    {
+        for(int j {upper_left_index_j}; j < right_edge; j++)
+        {
+            out_array[(bid * (array_length - (2 * reach))) + tid] += data_array[(i * array_length) + j];
+        }
+    }
+}
+
 int main()
 {
     int array_length {10};
@@ -107,14 +153,29 @@ int main()
 
     auto *data_array = new float[array_length * array_length];
     auto *out_array = new float[(array_length - (2 * reach)) * (array_length - (2 * reach))];
+    float *device_data_array, *device_out_array;
+    size_t bytes_data {(array_length * array_length) * sizeof(float)};
+    size_t bytes_out {((array_length - (2 * reach)) * (array_length - (2 * reach))) * sizeof(float)};
+    cudaMalloc(&device_data_array, bytes_data);
+    cudaMalloc(&device_out_array, bytes_out);
     populateArray(array_length, data_array, false);
     populateArray(array_length - (2 * reach), out_array, true);
+    cudaMemcpy(device_data_array, data_array, bytes_data, cudaMemcpyHostToDevice);
     printArray(array_length, data_array);
     calculateAnswer(array_length, reach, data_array, out_array);
     std::cout << "ANSWER" << std::endl;
     printArray(array_length - (2 * reach), out_array);
 
+    deviceCalculateAnswer<<<array_length - (2 * reach), array_length - (2 * reach)>>>(array_length, reach, device_data_array, device_out_array);
+    cudaDeviceSynchronize();
+    cudaMemcpy(out_array, device_out_array, bytes_out, cudaMemcpyDeviceToHost);
+    std::cout << "GPU ANSWER" << std::endl;
+    printArray(array_length - (2 * reach), out_array);
+
+
     delete[] data_array;
     delete[] out_array;
+    cudaFree(device_data_array);
+    cudaFree(device_out_array);
     return 0;
 }
